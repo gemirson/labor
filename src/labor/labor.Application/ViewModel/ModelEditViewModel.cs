0c269073b49fe4ae47eee6bf4508c858d8e0cfba
gemirson <gemirson.silva@gmail.com>
2020-08-02 20:22:02 -0300

    feature(Model): create ModelEditViewModel with ICommand

---
+++ src/labor/labor.Application/ViewModel/ModelEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation.Results;
+
+namespace labor.Application.ViewModel
+{
+    public class ModelEditViewModel
+    {
+        public int    Id { get; set; }
+        public string Name { get; set; }
+        public int    IdBrand { get; set; }
+       
+    }
+}
